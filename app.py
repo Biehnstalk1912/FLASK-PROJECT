d51b59116699fdd136679e694268abbd8975f88b
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-08-28 15:58:00 -0500

    Working graph

--- app.py
+++ app.py
@@ -5,31 +5,30 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.utils
 import json
 
 
 
-server = Flask(__name__)
-app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')   
-
+app = Flask(__name__)
 engine = create_engine("mysql+pymysql://root:@localhost/flask_project")
 query = "SELECT * FROM orders_table"
 df = pd.read_sql_query(query, engine)
 df = df.iloc[1:]
 df['Order_Date'] = pd.to_datetime(df['Order_Date'], format='%m-%d-%y')
 
-fig = px.bar(x=df['Order_Date'], y=df['Quantity'], labels={'x': 'Order Date', 'y': 'Quantity'}, title='Quantity by Order Date')
-
-app.layout = html.Div([
-    html.H1('Interactive Graph with Dash and Flask'),
-    dcc.Graph(figure=fig)
-])
 
 
-@server.route("/")
+
+@app.route("/")
 def index():
-    return render_template("index.html")
+    fig = px.bar(x=df['Order_Date'], y=df['Quantity'], 
+                 labels={'x': 'Order Date', 'y': 'Quantity'}, 
+                 title='Quantity by Order Date')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("index.html", graphJSON = graphJSON)
+
 
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    app.run(debug=True)
