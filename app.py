b558460ddac7d0f48103ab3480ac162c5e5f85b3
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-08-28 15:51:46 -0500

    checkpoint

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import json
 
 
 
@@ -13,6 +14,22 @@ app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
 
 engine = create_engine("mysql+pymysql://root:@localhost/flask_project")
 query = "SELECT * FROM orders_table"
+df = pd.read_sql_query(query, engine)
+df = df.iloc[1:]
+df['Order_Date'] = pd.to_datetime(df['Order_Date'], format='%m-%d-%y')
 
-df = pd.read_sql(query, engine)
-print(df.head(20))
+fig = px.bar(x=df['Order_Date'], y=df['Quantity'], labels={'x': 'Order Date', 'y': 'Quantity'}, title='Quantity by Order Date')
+
+app.layout = html.Div([
+    html.H1('Interactive Graph with Dash and Flask'),
+    dcc.Graph(figure=fig)
+])
+
+
+@server.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    server.run(debug=True)
