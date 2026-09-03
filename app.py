f092b55196a00b170de1f8367dbad154e6cb8a09
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-09-03 16:22:53 -0500

    Stopping for the day

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-import pandas as pd 
-from sqlalchemy import create_engine
 from flask import Flask, render_template
-import dash 
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import plotly.express as px
-import plotly.utils
+import pymysql
+import time
 import json
 
 
-
 app = Flask(__name__)
-engine = create_engine("mysql+pymysql://root:@localhost/flask_project")
-query = "SELECT * FROM orders_table"
-df = pd.read_sql_query(query, engine)
-df = df.iloc[1:]
-df['Order_Date'] = pd.to_datetime(df['Order_Date'], format='%m-%d-%y')
-
+def get_latest_mysql_date():
+    with pymysql.connect(host='localhost', user='root', password='', database='flask_project') as conn:
+        with conn.cursor() as cursor:  
+            cursor.execute("SELECT State, Quantity FROM orders_table ORDER BY Order_Date DESC LIMIT 50")
+            rows = cursor.fetchall()
+            return rows
+@app.route('/stream')
+def stream():
+    def generate():
+        while True:
+            rows = get_latest_mysql_date()
+            data = []
+            for row in rows:
+                data.append({
+                    'State': row[0],
+                    'Quantity': row[1]
+                })
+            yield f"data: {json.dumps(data)}\n\n"
+            time.sleep(5)
+    return app.response_class(generate(), mimetype='text/event-stream')
 
 
 
 @app.route("/")
 def index():
-    fig = px.bar(x=df['Order_Date'], y=df['Quantity'], 
-                 labels={'x': 'Order Date', 'y': 'Quantity'}, 
-                 title='Quantity by Order Date')
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template("index.html", graphJSON = graphJSON)
+    return render_template("index.html")
 
 
 
