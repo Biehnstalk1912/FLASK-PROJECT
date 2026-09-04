44e587f4bf1bc6fcfd41d069c17636afa68c2e7d
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-09-04 10:35:24 -0500

    Chart
    Fixed the sole backend error, and implemented the front-end.

--- app.py
+++ app.py
@@ -14,23 +14,26 @@ def index():
 def get_latest_mysql_date():
     with pymysql.connect(host='localhost', user='root', password='', database='flask_project') as conn:
         with conn.cursor() as cursor:  
-            cursor.execute("SELECT State, Quantity FROM orders_table ORDER BY Order_Date DESC LIMIT 50")
+            cursor.execute("SELECT State, COUNT(Quantity) as `State Count` FROM orders_table GROUP BY State ORDER BY `State Count` DESC")
             rows = cursor.fetchall()
             return rows
         
         
 @app.route('/stream')
+@app.route('/stream')
 def stream():
     def generate():
         while True:
             rows = get_latest_mysql_date()
-            data = []
             json_data = [{"State": row[0], "Quantity": row[1]} for row in rows]
-            data.append(json_data)
-            yield f"data: {json.dumps(data)}\n\n"
+            yield f"data: {json.dumps(json_data)}\n\n"
             time.sleep(3)
-    return app.response_class(generate(), mimetype='text/event-stream')
-
+    return app.response_class(generate(),
+                              mimetype='text/event-stream',
+                              headers={
+                                  'Cache-Control': 'no-cache',
+                                  'X-Accel-Buffering': 'no'
+                              })   
 
 
 
