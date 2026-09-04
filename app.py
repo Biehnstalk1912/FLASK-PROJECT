ce55ae5b96ae535d2ccbff553f35e81c5fde9bfe
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-09-04 09:49:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,32 +5,33 @@ import json
 
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Data connects
 def get_latest_mysql_date():
     with pymysql.connect(host='localhost', user='root', password='', database='flask_project') as conn:
         with conn.cursor() as cursor:  
             cursor.execute("SELECT State, Quantity FROM orders_table ORDER BY Order_Date DESC LIMIT 50")
             rows = cursor.fetchall()
             return rows
+        
+        
 @app.route('/stream')
 def stream():
     def generate():
         while True:
             rows = get_latest_mysql_date()
             data = []
-            for row in rows:
-                data.append({
-                    'State': row[0],
-                    'Quantity': row[1]
-                })
+            json_data = [{"State": row[0], "Quantity": row[1]} for row in rows]
+            data.append(json_data)
             yield f"data: {json.dumps(data)}\n\n"
-            time.sleep(5)
+            time.sleep(3)
     return app.response_class(generate(), mimetype='text/event-stream')
 
 
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 
 
